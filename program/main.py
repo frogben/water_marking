4e30a4d06cc5069bfd81d4fb319607cc312cf7f9
frogben <s71351015@gmail.com>
2018-12-11 01:52:41 +0800

    驗證還沒做好

--- program/main.py
+++ program/main.py
@@ -1,24 +1,26 @@
 import numpy as np
 import cv2
 import hashlib
+from operator import xor
 
 def cal_sigma(round_pix):
     sigma=0
-    for i,v in enumerate(round_pix):
-        if(i!=7):
-            diff=round_pix[i]-round_pix[i+1]
-            diff_squar=diff**2
-            sigma+=diff_squar
-        else:
-            diff=round_pix[7]-round_pix[0]
-            diff_squar=diff**2
-            sigma+=diff_squar
+
+    for i in range(7):
+        diff=int(round_pix[i])-int(round_pix[i+1])
+        diff_squar=diff**2
+        sigma+=diff_squar
+
+    diff=round_pix[7]-round_pix[0]
+    diff_squar=diff**2
+    sigma+=diff_squar
     if(sigma<8):
         return 2
     elif( (8<=sigma) and (sigma<16) ):
         return 3
     else:
         return 4
+
 def cal_hidden_hash(round_pix,block_index,ID_image,SK):
     all_str=""
     for i,v in enumerate(round_pix):
@@ -49,25 +51,43 @@ def cal_hidden_val(middle_pix_val,folded_feature,num_LSB):
         middle_pix_val=middle_pix_val & 248
     else:
         middle_pix_val=middle_pix_val & 240
-    middle_pix_val+=folded_feature
+    print('$$$$')
+    print('folded_feature:',folded_feature)
+    print('$$$$')
+    middle_pix_val+=int(folded_feature,2)
     return middle_pix_val
 
 
 def folder_feature(hashed_feature,num_LSB):
-    hashed_feature+="0000"
+    hashed_feature_bin=bin(int(hashed_feature,16))
+    hashed_feature_bin=hashed_feature_bin[2:]
+    hashed_feature_bin=hashed_feature_bin.rjust(128,'0')
+    hashed_feature_bin+="0000"
     bit_list=[]
-    for i in range(0,len(hashed_feature),num_LSB):
-	    bit_list.append(hashed_feature[i:i+num_LSB])
-    for i in range(len(bit_list)-1):
-        exclusive_or=int(bit_list[i],2)!=int(bit_list[i+1],2)
-        binary_exclusive_or=bin(exclusive_or)
-        binary_exclusive_or=binary_exclusive_or[2:]
-        bit_list[i+1]=binary_exclusive_or
-    return bit_list[len(bit_list)]
+  
+    for i in range(0,len(hashed_feature_bin),num_LSB):
+        bit_list.append(hashed_feature_bin[i:i+num_LSB])
+   
+    for j in range(len(bit_list)-1):
+        this_bit=int(bit_list[j],2)
+        next_bit=int(bit_list[j+1],2)
+        exclusive_or=this_bit^next_bit
+        exclusive_or_bin=bin(exclusive_or)
+        exclusive_or_bin=exclusive_or_bin[2:]
+        exclusive_or_bin=exclusive_or_bin.rjust(num_LSB,'0')
+        bit_list[j+1]=exclusive_or_bin
+    
+    return bit_list[len(bit_list)-1]
+
     
 
-##目前只有實作灰階
-def add_waterMarking(img_gray,out_path):
+
+"""
+目前只有實作灰階  
+out_path 
+
+"""
+def add_waterMarking(img_gray):
     
     img_shape=img_gray.shape
     #參數設定#
@@ -91,14 +111,18 @@ def add_waterMarking(img_gray,out_path):
             round_pix.append(img_gray[h+2,w+1])
             round_pix.append(img_gray[h+2,w+2])
             
+            
             num_LSB=cal_sigma(round_pix)
             hashed_feature=cal_hidden_hash(round_pix,block_index,ID_image,SK)
-            block_index+=1
+            
             middle_pix_val=img_gray[h+1,w+1]
             folded_feature=folder_feature(hashed_feature,num_LSB)
             img_gray[h+1,w+1]=cal_hidden_val(middle_pix_val,folded_feature,num_LSB)
+
+            block_index+=1
     
-    cv2.imwrite(out_path, img_gray,[cv2.IMWRITE_JPEG_QUALITY, 100])
+    return img_gray
+
 
 ##目前只有實作灰階
 def validate_waterMarking(img_gray):
@@ -127,6 +151,7 @@ def validate_waterMarking(img_gray):
             round_pix.append(img_gray[h+2,w+1])
             round_pix.append(img_gray[h+2,w+2])
             
+            
             num_LSB=cal_sigma(round_pix)
 
             middle_pix_val=img_gray[h+1,w+1]
@@ -137,20 +162,36 @@ def validate_waterMarking(img_gray):
                 LSB_feature=middle_pix_val & 7
             else:
                 LSB_feature=middle_pix_val & 15
-            
+            print(LSB_feature)
             
             hashed_feature=cal_hidden_hash(round_pix,block_index,ID_image,SK)
 
-            LSB_feature
-                    
 
-            
             block_index+=1
             
             img_gray[h+1,w+1]=cal_hidden_val(middle_pix_val,hashed_feature,num_LSB)
-    
+            break
+        break
     return flag
 
+
+# 輸入彩色圖片
 img_path='../data/lena.jpg'
+# 未認證的灰階圖片
+out_gray_path='../data/lena_gray.jpg'
+# 認證的灰階圖片
+out_encode_path='../data/lena_encode2.jpg'
+
+
+# 存檔未認證的灰階圖片
 img_gray = cv2.imread(img_path,cv2.IMREAD_GRAYSCALE)
-add_waterMarking(img_gray,'../data/output2.jpg')
+cv2.imwrite(out_gray_path, img_gray,[cv2.IMWRITE_JPEG_QUALITY, 100])
+
+
+#存檔認證過的灰階圖片
+img_encode=add_waterMarking(img_gray)
+cv2.imwrite(out_encode_path, img_encode,[cv2.IMWRITE_JPEG_QUALITY, 100])
+
+img_test = cv2.imread(out_encode_path,cv2.IMREAD_GRAYSCALE)
+validate_waterMarking(img_test)
+
